3540f4aa3504fa5bc9f710471ab4ef6d6124d9f7
Christi <christi@studylog.com>
2023-03-17 16:52:08 -0400

    Added init needed components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useCallback, useState, useRef } from 'react';
+
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Button from 'react-bootstrap/Button';
@@ -5,25 +7,30 @@ import Button from 'react-bootstrap/Button';
 // Will use this to import the xlsx file
 // import { read, writeFileXLSX } from "xlsx";
 
+export default function Importer() {
+  const [file, setFile] = useState(null);
 
-function App() {
+  const importFile = useCallback(() => {
+    console.log("Importing file");
+    console.log(file)
+
+  })
 
   return (
     <div className="App">
       <header className="App-header">
         <p> Let's import a xlsx file </p>
       </header>
+      
       <InputGroup className="mb-3">
+        <Form.Control type="file" onChange={(e) => setFile(e.target.files[0])}/>
         <Button 
           variant="outline-secondary" 
           id="button-addon1" 
-        >
-          Import File
-        </Button>
-        <Form.Control/>
+          onClick={importFile}
+        > Import File </Button>
       </InputGroup>
+      
     </div>
   );
 }
-
-export default App;
