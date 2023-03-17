d2debaa3bd6a905e4af7eeec1296c59f3422a43d
Christi <christi@studylog.com>
2023-03-17 13:47:07 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -7,21 +7,21 @@ import Button from 'react-bootstrap/Button';
 
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <p> Let's import a xlsx file </p>
       </header>
       <InputGroup className="mb-3">
-        <Button variant="outline-secondary" id="button-addon1">
+        <Button 
+          variant="outline-secondary" 
+          id="button-addon1" 
+        >
           Import File
         </Button>
         <Form.Control/>
       </InputGroup>
-      {/* <Form.Group controlId="formFileLg" className="mb-3">
-        <Form.Label>Large file input example</Form.Label>
-        <Form.Control type="file" size="lg" />
-      </Form.Group> */}
     </div>
   );
 }
